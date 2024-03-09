dd9365e0d2b907e985ef434463e95f3d17be3176
Thoufeeq2k <152467798+Thoufeeq2k@users.noreply.github.com>
2024-03-09 12:26:08 +0530

    Update Web.py

--- Web.py
+++ Web.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import soundfile as sf
 import numpy as np
 import tensorflow as tf
 import librosa
@@ -13,21 +12,19 @@ labelencoder.fit(class_labels)
 # Load your audio classifier model
 model = tf.keras.models.load_model('audio_classification.hdf5')
 
-def detect_audio():
-    file_path = st.file_uploader("Upload Audio File", type=["wav"])
-    if file_path is not None:
-        data, _ = sf.read(file_path)
-        audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast')
-        mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-        mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
+st.title("Audio Classification")
 
-        mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
+uploaded_file = st.file_uploader("Upload Audio File", type=["wav"])
 
-        predicted_label = np.argmax(model.predict(mfccs_scaled_features), axis=-1)
-        predicted_class = labelencoder.inverse_transform(predicted_label)[0]
+if uploaded_file is not None:
+    # Load the selected audio file
+    audio, sample_rate = librosa.load(uploaded_file, res_type='kaiser_fast')
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
 
-        st.write(f"Predicted Class: {predicted_class}")
+    mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
 
-st.title("Audio Classifier")
+    predicted_label = np.argmax(model.predict(mfccs_scaled_features), axis=-1)
+    predicted_class = labelencoder.inverse_transform(predicted_label)[0]
 
-detect_btn = st.button("Detect", on_click=detect_audio)
+    st.write(f"Predicted Class: {predicted_class}")
