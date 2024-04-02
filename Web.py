ada3e1b7f4d5382186f7d2ee3835658c4b7668fe
Thoufeeq2k <152467798+Thoufeeq2k@users.noreply.github.com>
2024-04-02 15:20:59 +0530

    Update Web.py

--- Web.py
+++ Web.py
@@ -40,7 +40,7 @@ if uploaded_file is not None:
 
 if uploaded_file is not None:
     # Load the selected audio file
-    audio, sample_rate = librosa.load(uploaded_file, res_type='kaiser_fast')
+    audio, sample_rate = librosa.load(wav_file, res_type='kaiser_fast')
     mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
     mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
     mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
